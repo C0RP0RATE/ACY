41b49c6d015e47bb97cc7a635dc87c61a14b2805
corporate <agencystest101@gmail.com>
2024-08-02 07:08:36 -0400

    Update ID.lua

--- ID.lua
+++ ID.lua
@@ -1,4 +1,77 @@
-_G.SDIKjFNIEJSMNFdiks = {
-    "D98AFA2B-079C-4C35-B2EC-43A552B61D60"
-}
-
+[
+    {
+        "key": "5090",
+        "hwid": "035E02D8-04D3-0504-F006-880700080009",
+        "discord_user_id": "1079148942022951022"
+    },
+    {
+        "key": "PercyX21023213",
+        "hwid": "F75E1FD8-A7D3-1102-793F-2B1D3A17F104",
+        "discord_user_id": "602924140386582740"
+    },
+    {
+        "key": "BopDev",
+        "hwid": "CB12E642-0A32-11ED-8955-07ABD4DD690A",
+        "discord_user_id": "771785002085908491"
+    },
+    {
+        "key": "Bopppington",
+        "hwid": "CB12E642-0A32-11ED-8955-07ABD4DD690A",
+        "discord_user_id": "771785002085908491"
+    },
+    {
+        "key": "nigger",
+        "hwid": "CB12E642-0A32-11ED-8955-07ABD4DD690A",
+        "discord_user_id": "771785002085908491"
+    },
+    {
+        "key": "niggers",
+        "hwid": "C3E159CC-BD00-11EC-9260-088E900A166B",
+        "discord_user_id": "830771328810221598"
+    },
+    {
+        "key": "nigeers",
+        "hwid": "C3E159CC-BD00-11EC-9260-088E900A166B",
+        "discord_user_id": "830771328810221598"
+    },
+    {
+        "key": "Roboppington",
+        "hwid": "CB12E642-0A32-11ED-8955-07ABD4DD690A",
+        "discord_user_id": "771785002085908491"
+    },
+    {
+        "key": "Roboppington",
+        "hwid": "CB12E642-0A32-11ED-8955-07ABD4DD690A",
+        "discord_user_id": "771785002085908491"
+    },
+    {
+        "key": "nigeria",
+        "hwid": "C3E159CC-BD00-11EC-9260-088E900A166B",
+        "discord_user_id": "830771328810221598"
+    },
+    {
+        "key": "nigeria",
+        "hwid": "C3E159CC-BD00-11EC-9260-088E900A166B",
+        "discord_user_id": "830771328810221598"
+    },
+    {
+        "key": "nigeria",
+        "hwid": "C3E159CC-BD00-11EC-9260-088E900A166B",
+        "discord_user_id": "830771328810221598"
+    },
+    {
+        "key": "nigeria",
+        "hwid": "C3E159CC-BD00-11EC-9260-088E900A166B",
+        "discord_user_id": "830771328810221598"
+    },
+    {
+        "key": "nigeria",
+        "hwid": "C3E159CC-BD00-11EC-9260-088E900A166B",
+        "discord_user_id": "830771328810221598"
+    },
+    {
+        "key": "NIGGERS",
+        "hwid": "CB12E642-0A32-11ED-8955-07ABD4DD690A",
+        "discord_user_id": "771785002085908491"
+    }
+]
